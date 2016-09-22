2128e5b4eb4ab5f9f0293a736547ba75ff674b2a
bill-cooper <william.l.cooper1@gmail.com>
2016-09-22 05:11:25 -0500

    Baseline skeleton

--- app/index.js
+++ app/index.js
@@ -1,26 +1,6 @@
+// APP INDEX
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Root from 'components/Root/Root'
-import configureStore from 'config/store'
-import { loadFromLocalStorage } from 'helpers/localStorage'
-import { users as actions } from 'actions';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
 
-const store = configureStore()
-//console.log(store)
-//TODO: remove these. these are just here temporarily to assist with debugging.
-//removeFromLocalStorage('auth')
-//removeFromLocalStorage('user')
-
-//If user has token, consider the user to be authenticated
-loadFromLocalStorage('auth')
-    // Update application state
-    ? store.dispatch(actions.fetchAndHandleAuthenticatedUser(loadFromLocalStorage('auth')))
-    : null
-
-ReactDOM.render(
-    <Root store={store} />,
-    document.getElementById('root')
-)
+// RENDER ROOT COMPONENT
