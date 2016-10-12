4e7e9c0f72cb43650d74e71b90724ec8c0a78c39
bill-cooper <william.l.cooper1@gmail.com>
2016-10-12 12:41:03 -0500

    Added delete confirmation dialog to connections

--- app/components/Connection.js
+++ app/components/Connection.js
@@ -1,36 +1,68 @@
-import React, { PropTypes } from 'react'
-import { Map } from 'immutable'
+import React, { PropTypes, Component } from 'react'
 import FlatButton from 'material-ui/FlatButton';
-import { ConnectionCard } from 'components'
+import { ConnectionCard, DialogConfirm } from 'components'
 import s from './Connection.css'
 
-function Connection(props) {
 
-    const renderCardActions = ({ deleteConnection, connection }) => {
-        return (
-            <div>
-                <FlatButton onClick={(event) => deleteConnection(event, connection)} label="Delete Connection" />
-            </div>
-        )
+class Connection extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDeleteConfirmation: false
+        }
+    }
+    openDeleteConfirmation = () => {
+        this.setState({
+            ... this.state,
+            showDeleteConfirmation: true
+        })
+    }
+    closeDeleteConfirmation = () => {
+        this.setState({
+            ... this.state,
+            showDeleteConfirmation: false
+        })
     }
 
-    return (
-        <ConnectionCard
-            className={s.connectionWrapper}
-            connectionUri={props.connection.connectionUri}
-            connectionType={props.connection.connectionType}
-            connectionName={props.connection.connectionName}
-            isCreating={props.connection.isCreating}
-            isDeleting={props.connection.isDeleting}
-            actions={renderCardActions(props)}
-        />
-    )
+    render () {
+        const renderCardActions = () => {
+            return (
+                <div>
+                    <FlatButton onClick={(event) => {event.stopPropagation(); this.openDeleteConfirmation();}} label="Delete Connection" />
+                </div>
+            )
+        }
+
+
+        return (
+                <div>
+                    <ConnectionCard
+                        className={s.connectionWrapper}
+                        connectionUri={this.props.connection.connectionUri}
+                        connectionType={this.props.connection.connectionType}
+                        connectionName={this.props.connection.connectionName}
+                        isCreating={this.props.connection.isCreating}
+                        isDeleting={this.props.connection.isDeleting}
+                        actions={renderCardActions()}
+                    />
+                    <DialogConfirm
+                        title = "Delete Connection"
+                        message = {"You are about to delete this connection.  This cannot be undone.  Are you sure?"}
+                        isOpen = {this.state.showDeleteConfirmation}
+                        confirmButtonText = "Yes, Delete"
+                        confirmCallback = {() => { this.closeDeleteConfirmation(); this.props.deleteConnection(this.props.connection); } }
+                        cancelCallback = {this.closeDeleteConfirmation}     
+                        />
+                </div>
+            )
+     }
 
 }
 
 Connection.propTypes = {
     connection: PropTypes.object.isRequired,
-    deleteConnection: PropTypes.func
+    deleteConnection: PropTypes.func.isRequired
 }
 
 export default Connection
--- app/containers/ConnectionContainer.js
+++ app/containers/ConnectionContainer.js
@@ -3,12 +3,10 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { Connection } from 'components'
 import { connectionActions as actions } from 'actions'
-import { getConnection } from '../reducers'
 
 class ConnectionContainer extends Component {
 
-    deleteConnection = (event, connection) => {
-        event.stopPropagation()
+    deleteConnection = (connection) => {
         this.props.connectionDeleteInitialize(connection)
         //redirect user to dashboard if we are in the details view
         if (this.props.router.isActive({ pathname: `/dashboard/apps/app/${this.props.connection.appId}/connections/connection/${this.props.connection.connectionId}`}))
